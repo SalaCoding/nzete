c7cc17afb7f3517b26fd8120c057a0caaf58ade8
Iyoke Sala <salaarnold14@gmail.com>
2026-04-30 20:26:55 +0100

    phone

--- mosisa-na-nse/src/constants/api.js
+++ mosisa-na-nse/src/constants/api.js
@@ -1,5 +1,5 @@
 import Constants from 'expo-constants';
 export const API_URL = Constants.expoConfig?.extra?.EXPO_PUBLIC_API_URL || "https://nzete.onrender.com";
-export const FRONTEND_URL = Constants.expoConfig?.extra?.EXPO_PUBLIC_FRONTEND_URL || "https://nzete.onrender.com/reset-password";
+export const FRONTEND_URL = Constants.expoConfig?.extra?.EXPO_PUBLIC_FRONTEND_URL || "https://nzete.onrender.com"; //"https://nzete.onrender.com/reset-password"
 
 console.log("Connecting to:", API_URL);
