3fdd2fc5645c698c3d9f05104a53a8ff7c977252
Iyoke Sala <salaarnold14@gmail.com>
2026-06-27 15:08:26 +0100

    babel

--- mosisa-na-nse/metro.config.js
+++ mosisa-na-nse/metro.config.js
@@ -1,4 +1,11 @@
 const { getDefaultConfig } = require('expo/metro-config');
+
 const config = getDefaultConfig(__dirname);
+
+// 1. Keep your existing database asset support
 config.resolver.assetExts.push('db');
+
+// 2. Add source extensions to resolve the 'import.meta' crash on web
+config.resolver.sourceExts = [...config.resolver.sourceExts, 'mjs', 'cjs'];
+
 module.exports = config;
