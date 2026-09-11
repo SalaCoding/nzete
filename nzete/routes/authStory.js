53c6732a7449cde39c9d92f8ea305259b2793a5e
SalaCoding <salaarnold14@gmail.com>
2026-09-11 19:40:27 +0100

    Fololo

--- nzete/routes/authStory.js
+++ nzete/routes/authStory.js
@@ -22,7 +22,7 @@ import {
   dislikeComment
 } from '../controllers/commentController.js';
 
-import { story as Sanza } from "../seed/seedSanza.js";
+import { story as Fololo } from "../seed/seedFololo.js";
 
 // Helper: find story by ObjectId or slug
 async function findStoryByIdOrSlug(idOrSlug) {
@@ -53,7 +53,7 @@ const answerLimiter = rateLimit({
   message: { error: 'Too many answer attempts, please slow down.' }
 });
 // --- CONFIGURE THE SEED STORY AND IMAGE ---
-const storyData = Sanza;
+const storyData = Fololo;
 const imagePath = path.resolve(storyData.imagePath);
 const storyTitle = storyData. title;
 const storySlug = slugify(storyTitle, { lower: true, strict: true });
