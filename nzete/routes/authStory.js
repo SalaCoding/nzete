f31c43923636a44fc9c3bfda380843bc947100a0
SalaCoding <salaarnold14@gmail.com>
2026-09-11 23:31:12 +0100

    new update

--- nzete/routes/authStory.js
+++ nzete/routes/authStory.js
@@ -123,7 +123,7 @@ async function seedStory() {
   }
 }
 
-seedStory()//I need to run this to add the story to the database, but I don't want to run it every time the server starts.
+//seedStory()//I need to run this to add the story to the database, but I don't want to run it every time the server starts.
 
 // ============================================================
 // BLOG ROUTES
--- nzete/seed/seedSanza.js
+++ nzete/seed/seedSanza.js
@@ -2,11 +2,11 @@ export const story = {
   title: "Sanza",
   content: `Ezalaki mokolo moko, bapekisaki batu bakende bilanga na mokolo ya Eyenga.
 
-    Kasi mama moko atosaki mibeko te.
+  Kasi mama moko atosaki mibeko te.
 
-    Na mokolo ya Eyenga, amemi mwana na ye ya muke na mokongo, azwi kitunga, kongo, pe na mbwa—bakeyi bilanga.
+  Na mokolo ya Eyenga, amemi mwana na ye ya muke na mokongo, azwi kitunga, kongo, pe na mbwa—bakeyi bilanga.
 
-    Elengé muasi abandi kobalola mabélé ti butu ekoti. Sanza eye kozwa ye.`,
+  Elengé muasi abandi kobalola mabélé ti butu ekoti. Sanza eye kozwa ye.`,
     imagePath: "public/images/sanza.png",
     audioUrl: "https://your-audio-file-url.mp3"
     };
